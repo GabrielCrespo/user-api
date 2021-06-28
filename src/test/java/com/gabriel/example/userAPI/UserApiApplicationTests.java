0956eff419ecfa01df050cb19e2e5a4cd33d9153
GabrielCrespo <gabriel.crespo1942@gmail.com>
2021-06-27 21:38:34 -0300

    userAPI Maven project

---
+++ src/test/java/com/gabriel/example/userAPI/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.example.userAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
